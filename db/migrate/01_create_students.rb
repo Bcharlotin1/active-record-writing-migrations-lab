a5cdb59fa62d6dda5ae0a9abaab4ff39ad9853bf
bcharlotin1 <biancacharlotin1@gmail.com>
2021-02-25 19:10:47 -0500

    commit tree 620bc0b7fef707f7388efcbacb5611cf684485fc

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+    def change
+        create_table :students do |t|
+        #Here we've added the create_table method and passed the name of the table we want to create as a symbol.
+        t.string :name
+   
+        end
+    end
 end
